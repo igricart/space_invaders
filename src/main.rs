b60cb0af7b6263f5cfbb3017b679b00248fedafa
Ignacio Ricart <ignacio@poli.ufrj.br>
2022-07-27 20:15:25 +0200

    add ferry sprite to system

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-
 use bevy::prelude::*;
+
+// Define player sprite
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
 
 fn main() {
     App::new()
-    .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
-    .insert_resource(WindowDescriptor {
-        title: "Bevy".to_string(),
-        width: 598.0,
-        height: 676.0,
-        ..Default::default()
-    })
-    .add_plugins(DefaultPlugins)
-    .run();
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Bevy".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run();
     println!("Hello, world!");
+}
+
+fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load(PLAYER_SPRITE),
+        ..Default::default()
+    });
 }
