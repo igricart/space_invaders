d72fd97160d9bf151d443066bfde9149a3c4f566
Ignacio Ricart <ignacio@poli.ufrj.br>
2022-07-27 21:59:37 +0200

    create player and velocity components to move our ferry

---
+++ src/components.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::Component;
+
+// Common components
+#[derive(Component)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+
+// Player Components
+#[derive(Component)]
+pub struct Player;
