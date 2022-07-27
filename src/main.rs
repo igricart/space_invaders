aa59bc61c97a2cd1d6f65b5b46d7aa92fc56da3f
Ignacio Ricart <ignacio@poli.ufrj.br>
2022-07-27 20:45:00 +0200

    add window size resource (globally unique data)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (144., 75.);
 const SPRITE_SCALE: f32 = 0.5;
 
+// Define Resource
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
@@ -28,6 +34,8 @@ fn setup_system(
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     let window = windows.get_primary_mut().unwrap();
     let (win_w, win_h) = (window.width(), window.height());
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
 
     window.set_position(IVec2::new(2780, 0));
 
