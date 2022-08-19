76dc02f7bb04f8eadebd9ba830db2f86fae0222f
Ignacio Ricart <ignacio@poli.ufrj.br>
2022-08-19 22:37:38 +0200

    create laser system

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,6 +1,6 @@
 use crate::{
-    components::{Enemy, SpriteSize},
-    EnemyCount, GameTextures, WinSize, ENEMY_MAX, ENEMY_SIZE, SPRITE_SCALE,
+    components::{Enemy, FromEnemy, Laser, Movable, SpriteSize, Velocity},
+    EnemyCount, GameTextures, WinSize, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, SPRITE_SCALE,
 };
 use bevy::{core::FixedTimestep, prelude::*};
 use rand::Rng;
@@ -12,7 +12,8 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(1.))
                 .with_system(enemy_spawn_system),
-        );
+        )
+        .add_system(enemy_fire_system);
     }
 }
 
@@ -45,5 +46,30 @@ fn enemy_spawn_system(
             .insert(SpriteSize::from(ENEMY_SIZE));
 
         enemy_count.0 += 1;
+    }
+}
+
+fn enemy_fire_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+) {
+    for &tf in enemy_query.iter() {
+        let (x, y) = (tf.translation.x, tf.translation.y);
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: game_textures.enemy_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y - 15., 0.),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+            .insert(FromEnemy)
+            .insert(Movable { auto_despawn: true })
+            .insert(Velocity { x: 0., y: -1. });
     }
 }
